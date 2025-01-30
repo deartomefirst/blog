41e7898a96d4e97bca55e66690226d181d9b113b
HYUNJOON KWAK <deartomefirst@gmail.com>
2025-01-30 15:37:16 +0900

    fix: catch error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,8 +62,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (e) {
-
+  } catch {
     return { message: 'Database Error: Failed to Create Invoice.'}
   }
   
